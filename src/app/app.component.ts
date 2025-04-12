73f30e0d1091acebc00e9de0e10b0db24fb4e0a7
Jonas Morales <jonasjosuemo2021@outlook.com>
2025-04-11 18:54:09 -0600

    Change ui library from TailwindCss to Ng Zorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzButtonModule, NzIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'APPLE-STORE-UI-ANGULAR';
+  isLoadingOne = false;
+  isLoadingTwo = false;
+
+  loadOne(): void {
+    this.isLoadingOne = true;
+    setTimeout(() => {
+      this.isLoadingOne = false;
+    }, 5000);
+  }
+
+  loadTwo(): void {
+    this.isLoadingTwo = true;
+    setTimeout(() => {
+      this.isLoadingTwo = false;
+    }, 5000);
+  }
 }
